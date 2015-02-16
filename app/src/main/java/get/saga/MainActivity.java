9efe5e9f80e1fb47f53f182d962e715d76edf394
Prempal Singh <prempal.42@gmail.com>
2015-02-17 00:24:46 +0530

    toolbar

--- app/src/main/java/get/saga/MainActivity.java
+++ app/src/main/java/get/saga/MainActivity.java
@@ -2,16 +2,28 @@ package get.saga;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    Toolbar mToolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String title = getString(R.string.app_name);
+        if (mToolbar == null) {
+            mToolbar = (Toolbar) findViewById(R.id.toolbar);
+            if (mToolbar != null) {
+                setSupportActionBar(mToolbar);
+                mToolbar.setTitle(title);
+                mToolbar.setTitleTextColor(getResources().getColor(R.color.white));
+            }
+        }
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.container, new DownloadFragment())
