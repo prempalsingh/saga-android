bc5b27f9d04c31b145f3980f4d6d24ee2ceda9db
Prempal Singh <prempal.42@gmail.com>
2015-03-03 00:41:46 +0530

    get json response for charts

--- app/src/main/java/get/saga/DownloadFragment.java
+++ app/src/main/java/get/saga/DownloadFragment.java
@@ -29,8 +29,15 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONArray;
+import org.json.JSONException;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -68,6 +75,7 @@ public class DownloadFragment extends Fragment {
                 startDownload();
             }
         });
+        new GetCharts().execute();
 
         return rootView;
     }
@@ -83,8 +91,7 @@ public class DownloadFragment extends Fragment {
                 new PostQuery().execute();
         }
         else
-            Toast.makeText(getActivity(),"Trouble connecting to the Internet",Toast.LENGTH_SHORT).show();
-
+            Toast.makeText(getActivity(),"Error connecting to the Internet",Toast.LENGTH_SHORT).show();
     }
 
     private class PostQuery extends AsyncTask<Void,Void,String> {
@@ -134,5 +141,65 @@ public class DownloadFragment extends Fragment {
             else
                 Toast.makeText(getActivity(),"Nothing found, sorry. Try another query?",Toast.LENGTH_SHORT).show();
         }
+    }
+
+    private class GetCharts extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected Void doInBackground(Void... arg0) {
+            URL url = null;
+            String jsonStr = null;
+            BufferedReader reader = null;
+            try {
+                url = new URL("http://boundbytech.com/saga/get_charts.php");
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                reader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                StringBuilder result = new StringBuilder();
+                String inputLine = "";
+                while ((inputLine = reader.readLine()) != null) {
+                    result.append(inputLine);
+                }
+                jsonStr = result.toString();
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try{
+                if(reader!=null)
+                    reader.close();
+                } catch (IOException e) {
+                e.printStackTrace();
+                }
+            }
+            Log.d("Response: ", "> " + jsonStr);
+            if (jsonStr != null) {
+                try {
+                    JSONArray jsonArray = new JSONArray(jsonStr);
+
+                    for(int i=0; i<jsonArray.length(); i++){
+                        JSONArray chart = jsonArray.getJSONArray(i);
+                        Log.d("Chart",chart.toString());
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                Log.e("Charts", "Couldn't get any data from the url");
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+        }
+
     }
 }
